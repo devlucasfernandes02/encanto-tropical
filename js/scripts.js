7192aeb6bf9d224645403964fa4dd802b7b77e6b
Lucas-Fernandes02 <alfds10@hotmail.com>
2024-10-11 16:59:53 -0300

    Responsividade quase concluida

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,23 @@
 const carrossel = document.querySelectorAll('.carrossel');
 const nextBtn = document.querySelector('.next');
 const prevBtn = document.querySelector('.prev');
+let btnMenu = document.querySelector('.btn-abrir-menu');
+let menu = document.querySelector('.menu-mobile');
+let overlay = document.querySelector('.overlay-menu');
 
 let currentCarrossel = 0;
+
+btnMenu.addEventListener('click', () => {
+    menu.classList.add('abrir-menu')
+});
+
+menu.addEventListener('click', () => {
+    menu.classList.remove('abrir-menu')
+});
+
+overlay.addEventListener('click', () => {
+    menu.classList.remove('abrir-menu')
+});
 
 function updateCarrossel() {
     const container = document.querySelector('.carrossel-container');
