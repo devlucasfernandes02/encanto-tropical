80bde68b890eed75ee6698015e19fc58c985bc0a
Lucas-Fernandes02 <alfds10@hotmail.com>
2024-11-28 12:31:49 -0300

    Adicionando Chatbot, com estilização e quase todo funcional.

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,16 @@ const prevBtn = document.querySelector('.prev');
 let btnMenu = document.querySelector('.btn-abrir-menu');
 let menu = document.querySelector('.menu-mobile');
 let overlay = document.querySelector('.overlay-menu');
+const chatInput = document.querySelector(".chat-input textarea")
+const sendChatBtn = document.querySelector(".chat-input span")
+const chatbox = document.querySelector(".chatbox")
+const chatbotToggler = document.querySelector(".chatbot-toggler")
+const chatbotCloseBtn = document.querySelector(".close-btn")
 
 let currentCarrossel = 0;
+let userMessage;
+const API_KEY = "AIzaSyCR7em8xl5yECIS731OstFKlSc2Ur7VXHA";
+const inputInitHeight = chatInput.scrollHeight;
 
 btnMenu.addEventListener('click', () => {
     menu.classList.add('abrir-menu')
@@ -36,6 +44,73 @@ prevBtn.addEventListener('click', () => {
 });
 
 window.addEventListener('resize', updateCarrossel);
+
+const createChatLi = (message, className) => {
+    const chatLi = document.createElement("li");
+    chatLi.classList.add("chat", className);
+    let chatContent = className === "outgoing" ? `<p></p>` : `<span class="material-symbols-outlined">smart_toy</span><p></p>`
+    chatLi.innerHTML = chatContent;
+    chatLi.querySelector("p").textContent = message;
+    return chatLi;
+}
+
+const generateResponse = (incomingChatLI) => {
+    const API_URL = `https://generativelanguage.googleapis.com/v1/models/gemini-1.5-pro:generateContent?key=${API_KEY}`;
+    const messageElement = incomingChatLI.querySelector("p");
+
+    const requestOptions = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ 
+          contents: [{ 
+            role: "user", 
+            parts: [{ text: userMessage }] 
+          }] 
+        }),
+      };
+
+      fetch(API_URL, requestOptions).then(res => res.json()).then(data => {
+        messageElement.textContent = data.candidates[0].content.parts[0].text;
+      }).catch((error) => {
+        messageElement.classList.add("error");
+        messageElement.textContent = "Oops! Something went wrong. Please try again.";
+      }).finally(() => chatbox.scrollTo(0, chatbox.scrollHeight));
+}
+
+const handleChat = () => {
+    userMessage = chatInput.value.trim();
+    if(!userMessage) return;
+    chatInput.value = "";
+    chatInput.style.height = `${inputInitHeight}px`;
+    
+    chatbox.appendChild(createChatLi(userMessage, "outgoing"));
+    chatbox.scrollTo(0, chatbox.scrollHeight);
+
+    setTimeout(() => {
+        const incomingChatLI = createChatLi("Thinking...", "incoming")
+        chatbox.appendChild( incomingChatLI);
+        chatbox.scrollTo(0, chatbox.scrollHeight);
+        generateResponse(incomingChatLI);
+    }, 600);
+
+}
+
+chatInput.addEventListener("input", () => {
+    chatInput.style.height = `${inputInitHeight}px`;
+    chatInput.style.height = `${chatInput.scrollHeight}px`;
+});
+
+chatInput.addEventListener("keydown", (e) => {
+    if(e.key === "Enter" && !e.shiftKey && window.innerWidth > 800) {
+        e.preventDefault();
+        handleChat();
+    }
+});
+
+chatbotCloseBtn.addEventListener("click", () => document.body.classList.remove("show-chatbot"))
+chatbotToggler.addEventListener("click", () => document.body.classList.toggle("show-chatbot"))
+
+sendChatBtn.addEventListener("click", handleChat);
 
 // Variáveis globais para o carrinho
 let cart = [];
